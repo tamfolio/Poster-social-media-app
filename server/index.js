f6851ff66597231a05e3080dd36f3e1ea3475cc1
Tamilore <tamiloreolaniyi@gmail.com>
2023-01-29 22:37:56 +0100

    initial setup

--- server/index.js
+++ server/index.js
@@ -11,8 +11,8 @@ import {fileURLToPath} from "url"
 
 
 /* CONFIGURATIONS */
-const __filename = fileURLToPath(import.meta,url);
-const dirname = path.dirname(__filename); 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename); 
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -34,3 +34,14 @@ const storage = multer.diskStorage({
 })
 
 const upload = multer({ storage })
+
+
+// MONGOOSE
+
+const PORT = process.env.PORT || 6001
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+}). catch((error) => console.log(`${error} did not connect`))
