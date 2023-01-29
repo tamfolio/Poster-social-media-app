ee1215868c2e49114b7e1be6ae04d80cc23b44be
Tamilore <tamiloreolaniyi@gmail.com>
2023-01-29 21:19:40 +0100

    file storage

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ import {fileURLToPath} from "url"
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta,url);
-const dirname = path.dirname(__filename);
+const dirname = path.dirname(__filename); 
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -22,3 +22,15 @@ app.use(bodyParser.json({limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")))
+
+/* FILE STORAGE */
+const storage = multer.diskStorage({
+    destination: function( req, file, cb){
+        cb(null, "public/assets");
+    },
+    filename: function(req,file,cb){
+        cb(null, file.originalname);
+    }
+})
+
+const upload = multer({ storage })
