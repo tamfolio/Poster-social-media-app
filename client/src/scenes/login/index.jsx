cc1fb7f062c08af3e4d5d1db2aed1239abbfc5dc
Tamilore <tamiloreolaniyi@gmail.com>
2023-02-16 12:10:26 +0100

    login page

--- client/src/scenes/login/index.jsx
+++ client/src/scenes/login/index.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from './Form'
 
 function Login() {
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px");
   return (
-    <div>Login</div>
-  )
+    <Box>
+      <Box width="100%" backgroundColor={theme.palette.background.alt} p="1rem 6%" textAlign="center">
+        <Typography fontWeight="bold" fontSize="32px" color="primary">
+          BestOdds
+        </Typography>
+      </Box>
+      <Box width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={theme.palette.background.alt}
+      >
+        <Typography fontWeight="500" variant='h5' sx={{ mb: "1.5rem"}}>
+            Welcome to Best Odds, Social Media space for chronic Sport Bet players
+        </Typography>
+        <Form/>
+      </Box>
+    </Box>
+  );
 }
 
-export default Login
+export default Login;
