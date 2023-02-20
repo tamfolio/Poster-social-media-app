5051e38f51f29dd17faaaf0c7662fc71f1dd98d8
Tamilore <tamiloreolaniyi@gmail.com>
2023-02-20 14:09:53 +0100

    user widget complete

--- client/src/scenes/homepage/index.jsx
+++ client/src/scenes/homepage/index.jsx
@@ -1,13 +1,38 @@
-import { Box } from '@mui/material'
-import React from 'react'
-import Navbar from 'scenes/navbar'
+import { Box, useMediaQuery } from "@mui/material";
+import React from "react";
+import { useSelector } from "react-redux";
+import Navbar from "scenes/navbar";
+import UserWidget from "scenes/widgets/UserWidget";
+import MyPostWidget from 'scenes/widgets/MyPostWidget'
 
 function HomePage() {
+  const isNonMobileScreens = useMediaQuery("min-width: 1000px");
+  const { _id, picturePath } = useSelector((state) => state.user);
   return (
     <Box>
-      <Navbar/>
+      <Navbar />
+      <Box
+        width="100%"
+        padding="2rem 6%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap="0.5rem"
+        justifyContent="space-between"
+      >
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+          <UserWidget userId={_id} picturePath={picturePath} />
+        </Box>
+        <Box
+          flexBasis={isNonMobileScreens ? "42%" : undefined}
+          mt={isNonMobileScreens ? undefined : "2rem"}
+        >
+          <MyPostWidget picturePath={picturePath}/>
+        </Box>
+       {isNonMobileScreens && (
+        <Box flexBasis="26%"></Box>
+       )}
+      </Box>
     </Box>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
