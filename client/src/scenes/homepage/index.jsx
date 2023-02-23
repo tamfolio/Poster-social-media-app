edd6069b4c521f87cdc18c666eba6b492a22b16b
Tamilore <tamiloreolaniyi@gmail.com>
2023-02-23 16:45:30 +0100

    posts and likes

--- client/src/scenes/homepage/index.jsx
+++ client/src/scenes/homepage/index.jsx
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux";
 import Navbar from "scenes/navbar";
 import UserWidget from "scenes/widgets/UserWidget";
 import MyPostWidget from 'scenes/widgets/MyPostWidget'
+import PostWidget from "scenes/widgets/PostWidget";
 
 function HomePage() {
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
@@ -26,6 +27,7 @@ function HomePage() {
           mt={isNonMobileScreens ? undefined : "2rem"}
         >
           <MyPostWidget picturePath={picturePath} />
+          <PostWidget userId={_id}/>
         </Box>
        {isNonMobileScreens && (
         <Box flexBasis="26%"></Box>
